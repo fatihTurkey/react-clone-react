300460a30f88e5ffd91e3b2b880cdbe2f657f582
fatihTurkey <40725166+fatihTurkey@users.noreply.github.com>
2023-02-19 01:57:20 +0300

    create sign in, sign up and forgot password components

---
+++ src/components/OAuth.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react"
+
+function OAuth() {
+  return (
+    <button className="w-full bg-red-600 my-3 text-white py-4 px-7 font-bold  uppercase" type="submit">
+      Continue With Google
+    </button>
+  )
+}
+
+export default OAuth
