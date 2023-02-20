994d9e7b95b8d272aa5a9ef511382bd9791d50f6
fatihTurkey <40725166+fatihTurkey@users.noreply.github.com>
2023-02-20 18:28:36 +0300

    Create Listing page copleted for database

--- src/App.js
+++ src/App.js
@@ -29,7 +29,9 @@ function App() {
           <Route path="/sign-up" element={<SignUp />}></Route>
           <Route path="/forgot-password" element={<ForgotPassword />}></Route>
           <Route path="/offers" element={<Offers />}></Route>
-          <Route path="/create-listing" element={<CreateListing />}></Route>
+          <Route path="/create-listing" element={<PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />}></Route>
+          </Route>
         </Routes>
       </Router>
       <ToastContainer position="bottom-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="dark" />
